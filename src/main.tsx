1030f3b57d4e357d0583816a88d11ecdf72dcda6
ayoubjd <46287719+ayoubjd@users.noreply.github.com>
2026-09-03 23:24:02 +0100

    commit

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { seedDatabase } from "./lib/db";
-
-seedDatabase().catch(console.error);
-
-createRoot(document.getElementById("root")!).render(<App />);
